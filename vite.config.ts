1bb384a8b39645b3b2aaf3c60bba81ca32274010
hehhhuhmm <133074138+hehhhuhmm@users.noreply.github.com>
2026-05-01 18:36:08 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
+  base: '/DermaCare---AI-Project/', 
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
